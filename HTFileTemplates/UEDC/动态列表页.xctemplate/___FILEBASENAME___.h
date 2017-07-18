744645a60f358d7ac609992b8851c34eb3c815f1
陶泽宇 <hztaozeyu@corp.netease.com>
2017-07-18 14:27:33 +0800

    添加 动态列表页面模版

---
+++ HTFileTemplates/UEDC/动态列表页.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,12 @@
+//
+// ___FILEBASENAMEASIDENTIFIER___.h
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___VARIABLE_BaseTableViewController:identifier___.h"
+
+@interface  ___VARIABLE_ControllerName:identifier___: ___VARIABLE_BaseTableViewController:identifier___
+
+@end
---
+++ HTFileTemplates/UEDC/动态列表页.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,95 @@
+//
+//  ___VARIABLE_ControllerName:identifier___.m
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+#import "___VARIABLE_ControllerName:identifier___.h"
+#import "___VARIABLE_CellModelClass:identifier___.h"
+#import "___VARIABLE_CellType:identifier___.h"
+#import "UITableView+FDTemplateLayoutCell.h"
+#import "HTControllerRouter.h"
+#import "HTNavigationPanProtocol.h"
+
+
+@interface ___VARIABLE_ControllerName:identifier___ ()  <HTRouteTargetProtocol,
+                                                    HTContainerViewControllerProtocol,
+                                                    HTNavigationBackPanGestureProtocol>
+
+@end
+
+@implementation ___VARIABLE_ControllerName:identifier___
+
+@synthesize containerController;
+
+#pragma mark - Router
+
++ (HTControllerRouterConfig*)configureRouter {
+    HT_EXPORT();
+    HTControllerRouterConfig *config = [[HTControllerRouterConfig alloc] initWithUrlPath:[self urlPath]];
+    return config;
+}
+
++ (NSString*)urlPath {
+    //to do: 定义改页面的urlPath
+    
+}
+
+- (void)receiveRoute:(HTControllerRouteParam*)param {
+    //to do: 接收并处理外部传参
+}
+
+
+
+#pragma mark --- Life Cycle ---
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //to do: 设置页面title
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    // 若要修改tableview／refreshview／loadmoreview的frame，在这里修改
+}
+
+#pragma mark --- Override BaseTableViewController Methods ---
+
+/**
+ 初始化HTTableViewDataSourceDelegate配置，tableview注册cell。 （refreshView/loadmoreView/statusView均已由基类完成）
+ */
+- (void)loadSubviews{
+    [super loadSubviews];
+    
+    // HTTableViewDataSourceDelegate 的详细使用请参考模板说明文档
+    self.dataSource =
+    [HTTableViewDataSourceDelegate dataSourceWithModel:self.listDatas
+                                           cellTypeMap:@{NSStringFromClass([___VARIABLE_CellModelClass:identifier___ class]): NSStringFromClass([___VARIABLE_CellType:identifier___ class])}
+                                     tableViewDelegate:self
+                                     cellConfiguration:^(___VARIABLE_CellType:identifier___ *cell, id model, NSIndexPath *indexPath) {
+                                         cell.fd_enforceFrameLayout = YES;
+                                         cell.model = model;
+                                     }];
+    [self.tableView registerClass:[___VARIABLE_CellType:identifier___ class] forCellReuseIdentifier:NSStringFromClass([___VARIABLE_CellType:identifier___ class])];
+    self.tableView.delegate = self.dataSource;
+    self.tableView.dataSource = self.dataSource;
+}
+
+- (HTBaseRequest *)configRequest{
+   //to do: 初始化该页面对应的请求接口
+}
+
+- (void)onSuccessResponseWithData:(RKMappingResult *)mappingResult
+                    withOperation:(RKObjectRequestOperation *)opration{
+   //to do: 处理成功回调的数据
+}
+
+#pragma mark --- UITableViewDelegate ---
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    // to do: cell clicked handle
+}
+@end
+
